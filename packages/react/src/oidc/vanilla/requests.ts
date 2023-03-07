63ba8533ff5e4dd6f21fa202535b79774287e412
Philip Hess <phess@kloveair1.com>
2023-03-07 09:16:22 -0500

    updates

--- packages/react/src/oidc/vanilla/requests.ts
+++ packages/react/src/oidc/vanilla/requests.ts
@@ -5,117 +5,149 @@ import { parseOriginalTokens } from './parseTokens';
 import { StringMap } from './types';
 
 const oneHourSecond = 60 * 60;
-export const fetchFromIssuer = async (openIdIssuerUrl: string, timeCacheSecond = oneHourSecond, storage = window.sessionStorage):
-    Promise<OidcAuthorizationServiceConfiguration> => {
-    const fullUrl = `${openIdIssuerUrl}/.well-known/openid-configuration`;
+export const fetchFromIssuer = async (
+  openIdIssuerUrl: string,
+  tenantId?: string,
+  timeCacheSecond = oneHourSecond,
+  storage = window.sessionStorage
+): Promise<OidcAuthorizationServiceConfiguration> => {
+  const fullUrl = `${openIdIssuerUrl}/.well-known/openid-configuration${
+    tenantId ? `/${tenantId}` : ''
+  }}`;
 
-    const localStorageKey = `oidc.server:${openIdIssuerUrl}`;
-    const data = getFromCache(localStorageKey, storage, timeCacheSecond);
-    if (data) {
-        return new OidcAuthorizationServiceConfiguration(data);
-    }
-    const response = await fetch(fullUrl);
+  const localStorageKey = `oidc.server:${openIdIssuerUrl}`;
+  const data = getFromCache(localStorageKey, storage, timeCacheSecond);
+  if (data) {
+    return new OidcAuthorizationServiceConfiguration(data);
+  }
+  const response = await fetch(fullUrl);
 
-    if (response.status !== 200) {
-        return null;
-    }
+  if (response.status !== 200) {
+    return null;
+  }
 
-    const result = await response.json();
+  const result = await response.json();
 
-    setCache(localStorageKey, result, storage);
-    return new OidcAuthorizationServiceConfiguration(result);
+  setCache(localStorageKey, result, storage);
+  return new OidcAuthorizationServiceConfiguration(result);
 };
 
-const internalFetch = async (url, headers, timeoutMs = 10000, numberRetry = 0) => {
-    let response;
-    try {
-        const controller = new AbortController();
-        setTimeout(() => controller.abort(), timeoutMs);
-        response = await fetch(url, { ...headers, signal: controller.signal });
-    } catch (e) {
-        if (e.message === 'AbortError' ||
-            e.message === 'Network request failed') {
-            if (numberRetry <= 1) {
-                return await internalFetch(url, headers, timeoutMs, numberRetry + 1);
-            } else {
-                throw e;
-            }
-        } else {
-            console.error(e.message);
-            throw e; // rethrow other unexpected errors
-        }
+const internalFetch = async (
+  url,
+  headers,
+  timeoutMs = 10000,
+  numberRetry = 0
+) => {
+  let response;
+  try {
+    const controller = new AbortController();
+    setTimeout(() => controller.abort(), timeoutMs);
+    response = await fetch(url, { ...headers, signal: controller.signal });
+  } catch (e) {
+    if (e.message === 'AbortError' || e.message === 'Network request failed') {
+      if (numberRetry <= 1) {
+        return await internalFetch(url, headers, timeoutMs, numberRetry + 1);
+      } else {
+        throw e;
+      }
+    } else {
+      console.error(e.message);
+      throw e; // rethrow other unexpected errors
     }
-    return response;
+  }
+  return response;
 };
 
 export const TOKEN_TYPE = {
-    refresh_token: 'refresh_token',
-    access_token: 'access_token',
+  refresh_token: 'refresh_token',
+  access_token: 'access_token',
 };
 
-export const performRevocationRequestAsync = async (url, token, token_type = TOKEN_TYPE.refresh_token, client_id, timeoutMs = 10000) => {
-    const details = {
-        token,
-        token_type_hint: token_type,
-        client_id,
-    };
+export const performRevocationRequestAsync = async (
+  url,
+  token,
+  token_type = TOKEN_TYPE.refresh_token,
+  client_id,
+  timeoutMs = 10000
+) => {
+  const details = {
+    token,
+    token_type_hint: token_type,
+    client_id,
+  };
 
-    const formBody = [];
-    for (const property in details) {
-        const encodedKey = encodeURIComponent(property);
-        const encodedValue = encodeURIComponent(details[property]);
-        formBody.push(`${encodedKey}=${encodedValue}`);
-    }
-    const formBodyString = formBody.join('&');
+  const formBody = [];
+  for (const property in details) {
+    const encodedKey = encodeURIComponent(property);
+    const encodedValue = encodeURIComponent(details[property]);
+    formBody.push(`${encodedKey}=${encodedValue}`);
+  }
+  const formBodyString = formBody.join('&');
 
-    const response = await internalFetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        },
-        body: formBodyString,
-    }, timeoutMs);
-    if (response.status !== 200) {
-        return { success: false };
-    }
-    return {
-        success: true,
-    };
+  const response = await internalFetch(
+    url,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      body: formBodyString,
+    },
+    timeoutMs
+  );
+  if (response.status !== 200) {
+    return { success: false };
+  }
+  return {
+    success: true,
+  };
 };
 
-export const performTokenRequestAsync = async (url, details, extras, oldTokens, tokenRenewMode: string, timeoutMs = 10000) => {
-    for (const [key, value] of Object.entries(extras)) {
-        if (details[key] === undefined) {
-            details[key] = value;
-        }
+export const performTokenRequestAsync = async (
+  url,
+  details,
+  extras,
+  oldTokens,
+  tokenRenewMode: string,
+  timeoutMs = 10000
+) => {
+  for (const [key, value] of Object.entries(extras)) {
+    if (details[key] === undefined) {
+      details[key] = value;
     }
+  }
 
-    const formBody = [];
-    for (const property in details) {
-        const encodedKey = encodeURIComponent(property);
-        const encodedValue = encodeURIComponent(details[property]);
-        formBody.push(`${encodedKey}=${encodedValue}`);
-    }
-    const formBodyString = formBody.join('&');
+  const formBody = [];
+  for (const property in details) {
+    const encodedKey = encodeURIComponent(property);
+    const encodedValue = encodeURIComponent(details[property]);
+    formBody.push(`${encodedKey}=${encodedValue}`);
+  }
+  const formBodyString = formBody.join('&');
 
-    const response = await internalFetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        },
-        body: formBodyString,
-    }, timeoutMs);
-    if (response.status !== 200) {
-        return { success: false, status: response.status };
-    }
-    const tokens = await response.json();
-    return {
-        success: true,
-        data: parseOriginalTokens(tokens, oldTokens, tokenRenewMode),
-    };
+  const response = await internalFetch(
+    url,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      },
+      body: formBodyString,
+    },
+    timeoutMs
+  );
+  if (response.status !== 200) {
+    return { success: false, status: response.status };
+  }
+  const tokens = await response.json();
+  return {
+    success: true,
+    data: parseOriginalTokens(tokens, oldTokens, tokenRenewMode),
+  };
 };
 
-export const performAuthorizationRequestAsync = (storage: any) => async (url, extras: StringMap) => {
+export const performAuthorizationRequestAsync =
+  (storage: any) => async (url, extras: StringMap) => {
     extras = extras ? { ...extras } : {};
     const codeVerifier = generateRandom(128);
     const codeChallenge = await deriveChallengeAsync(codeVerifier);
@@ -125,45 +157,54 @@ export const performAuthorizationRequestAsync = (storage: any) => async (url, ex
     extras.code_challenge_method = 'S256';
     let queryString = '';
     if (extras) {
-        for (const [key, value] of Object.entries(extras)) {
-            if (queryString === '') {
-                queryString += '?';
-            } else {
-                queryString += '&';
-            }
-            queryString += `${key}=${encodeURIComponent(value)}`;
+      for (const [key, value] of Object.entries(extras)) {
+        if (queryString === '') {
+          queryString += '?';
+        } else {
+          queryString += '&';
         }
+        queryString += `${key}=${encodeURIComponent(value)}`;
+      }
     }
     window.location.href = `${url}${queryString}`;
-};
+  };
 
-export const performFirstTokenRequestAsync = (storage:any) => async (url, extras, tokenRenewMode: string, timeoutMs = 10000) => {
+export const performFirstTokenRequestAsync =
+  (storage: any) =>
+  async (url, extras, tokenRenewMode: string, timeoutMs = 10000) => {
     extras = extras ? { ...extras } : {};
     extras.code_verifier = await storage.getCodeVerifierAsync();
     const formBody = [];
     for (const property in extras) {
-        const encodedKey = encodeURIComponent(property);
-        const encodedValue = encodeURIComponent(extras[property]);
-        formBody.push(`${encodedKey}=${encodedValue}`);
+      const encodedKey = encodeURIComponent(property);
+      const encodedValue = encodeURIComponent(extras[property]);
+      formBody.push(`${encodedKey}=${encodedValue}`);
     }
     const formBodyString = formBody.join('&');
-    const response = await internalFetch(url, {
+    const response = await internalFetch(
+      url,
+      {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
         },
         body: formBodyString,
-    }, timeoutMs);
-    await Promise.all([storage.setCodeVerifierAsync(null), storage.setStateAsync(null)]);
+      },
+      timeoutMs
+    );
+    await Promise.all([
+      storage.setCodeVerifierAsync(null),
+      storage.setStateAsync(null),
+    ]);
     if (response.status !== 200) {
-        return { success: false, status: response.status };
+      return { success: false, status: response.status };
     }
     const tokens = await response.json();
     return {
-        success: true,
-        data: {
-            state: extras.state,
-            tokens: parseOriginalTokens(tokens, null, tokenRenewMode),
-            },
+      success: true,
+      data: {
+        state: extras.state,
+        tokens: parseOriginalTokens(tokens, null, tokenRenewMode),
+      },
     };
-};
+  };
